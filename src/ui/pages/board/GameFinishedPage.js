de87ffdd681f8cb553d349022058c7a5e991cdd9
roman janko <RomanJanko@seznam.cz>
2018-01-14 12:38:56 +0100

    Restyling game finished page.

--- src/ui/pages/board/GameFinishedPage.js
+++ src/ui/pages/board/GameFinishedPage.js
@@ -24,27 +24,25 @@ class GameFinishedPage extends PureComponent {
       const { player, difficulty, time, hints } = this.props;
 
       return (
-         <div className="board-page-layout">
-            <div className="board">
-               <div className="game-finished-page">
-                  <div className="game-finished-page__heading">
-                     Congratulations, {player}!
-                  </div>
-                  <div className="game-finished-page__par">
-                     You finished your {difficulty} game in {numeral(time).format("00:00:00")} with
-                     {hints === 0 && " no hint used"}
-                     {hints === 1 && " only one hint used"}
-                     {hints >= 2 && ` ${hints} hints used`}
-                     .
-                  </div>
-                  <div className="game-finished-page__buttons">
-                     <div className="button game-finished-page__button">
-                        <Link to="/menu/leaderboard" className="button__link">Leaderboard</Link>
-                     </div>
-                     <div className="button game-finished-page__button">
-                        <Link to="/" className="button__link">New Game</Link>
-                     </div>
-                  </div>
+         <div className="game-finished-page">
+            <div className="game-finished-page__heading">
+               Congratulations, {player}!
+            </div>
+            <div className="game-finished-page__text">
+               You finished your game 
+               in {numeral(time).format("00:00:00")} with
+               {hints === 0 && " no hint used"}
+               {hints === 1 && " only one hint used"}
+               {hints >= 2 && ` ${hints} hints used`}
+               .
+               The game difficulty was {difficulty}.
+            </div>
+            <div className="game-finished-page__buttons">
+               <div className="button game-finished-page__button">
+                  <Link to="/menu/leaderboard" className="button__link">Leaderboard</Link>
+               </div>
+               <div className="button game-finished-page__button">
+                  <Link to="/" className="button__link">New Game</Link>
                </div>
             </div>
          </div>
